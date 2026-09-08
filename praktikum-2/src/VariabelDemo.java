9ef054fc2e5ffc1a5906cdd739bc1a48c091397c
ahmadazkasulthani <ahmadazkaach@gmail.com>
2026-09-08 11:21:29 +0700

    B.3.praktikan operator aritmatika

--- praktikum-2/src/VariabelDemo.java
+++ praktikum-2/src/VariabelDemo.java
@@ -1,13 +1,1 @@
-public class VariabelDemo {
-    public static void main(String[] args) {
-        int nilaiBulat = 9;
-        double nilaiDouble = nilaiBulat; //widening otomatis
-        System.out.println("widening: " + nilaiDouble);
 
-        double pecahan = 9.8;
-        int hasilCasting = (int) pecahan; //narrowing eksplisit
-        System.out.println("narrowing: " + hasilCasting);
-
-
-    }
-}
