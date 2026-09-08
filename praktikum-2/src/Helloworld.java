c554956f57a80af10424854632414ae0782f2a3d
ahmadazkasulthani <ahmadazkaach@gmail.com>
2026-09-08 09:03:37 +0700

    A.2. Struktur dasar program java

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -1,7 +1,9 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Nama");
         System.out.println("Ahmad Azka sulthani");
+        System.out.println();
+        System.out.println("ini baris baru");
 
     }
 }
