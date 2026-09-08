4d03204fc2e69b4b80fe5b8995773968a1386692
ahmadazkasulthani <ahmadazkaach@gmail.com>
2026-09-08 10:52:52 +0700

    A.3. komentar

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -5,5 +5,12 @@ public class Helloworld {
         System.out.println();
         System.out.println("ini baris baru");
 
+        //program menampilkan biodata sederhana
+        System.out.println("nama: azka");
+        /* baris bawah ini mencetak nim mahasiswa
+
+         */
+        System.out.println("nim:2025573010069`");
+
     }
 }
