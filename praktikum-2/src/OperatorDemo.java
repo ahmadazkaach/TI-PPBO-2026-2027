9817fe0d1d46797989686acb74fc211f7ef73744
ahmadazkasulthani <ahmadazkaach@gmail.com>
2026-09-08 11:28:56 +0700

    B.3.praktikan operator relasional dan logika

--- praktikum-2/src/OperatorDemo.java
+++ praktikum-2/src/OperatorDemo.java
@@ -1,11 +1,11 @@
 public class OperatorDemo {
     public static void main(String[] args) {
-        int a = 17, b = 5;
-        System.out.println("jumlah: "+(a + b));
-        System.out.println("selisih: "+(a - b));
-        System.out.println("kali: "+(a * b));
-        System.out.println("bagi: "+(a / b));
-        System.out.println("sisa bagi: "+(a % b));
+        int nilaiUjian = 80;
+        boolean hadir = true;
+        System.out.print("lulus (>=75):"+(nilaiUjian >=75));
+        System.out.print("lulus DAN hadir: "+(nilaiUjian<=75 && hadir));
+
+
 
 
 
