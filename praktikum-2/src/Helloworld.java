d96ebf5e5f10bf74d0e33a569148468e5fa24d6a
ahmadazkasulthani <ahmadazkaach@gmail.com>
2026-09-08 11:12:01 +0700

    B.2.casting(konversi tipe data)

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -1,16 +1,15 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Nama");
-        System.out.println("Ahmad Azka sulthani");
-        System.out.println();
-        System.out.println("ini baris baru");
+        String nama = "azka";
+        int umur = 19;
+        double tinggi = 168;
+        char goldarah = 'O';
+        boolean mahasiswaaktif = true;
 
-        //program menampilkan biodata sederhana
-        System.out.println("nama: azka");
-        /* baris bawah ini mencetak nim mahasiswa
-
-         */
-        System.out.println("nim:2025573010069`");
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Gol darah: " + goldarah);
+        System.out.println("aktif: " + mahasiswaaktif);
 
     }
 }
--- praktikum-2/src/VariabelDemo.java
+++ praktikum-2/src/VariabelDemo.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class VariabelDemo {
+    public static void main(String[] args) {
+        int nilaiBulat = 9;
+        double nilaiDouble = nilaiBulat; //widening otomatis
+        System.out.println("widening: " + nilaiDouble);
+
+        double pecahan = 9.8;
+        int hasilCasting = (int) pecahan; //narrowing eksplisit
+        System.out.println("narrowing: " + hasilCasting);
+
+
+    }
 }
